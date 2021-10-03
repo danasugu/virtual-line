1f1b6126d5cba361daf82099189c73bb504de4da
Dana Sugu <dana.sugu@gmail.com>
2021-10-03 14:17:36 +0300

    update

--- application/models/Orders_model.php
+++ application/models/Orders_model.php
@@ -3,8 +3,4 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Orders_model extends CI_Model {
 
-	public function index()
-	{
-		$this->load->view('dashboard');
-	}
 }
--- application/models/User_model.php
+++ application/models/User_model.php
@@ -2,9 +2,11 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class User_model extends CI_Model {
-
-	public function index()
-	{
-		$this->load->view('dashboard');
-	}
+ 
+  // public function getMessages()
+  // {
+  //   $this->db->get('messages');
+  //   $query = $this->result();
+  //   return $query;
+  // }
 }
