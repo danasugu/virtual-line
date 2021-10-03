a72bab687ab209fe5505cb6bc72d00d5a13bd810
Dana Sugu <dana.sugu@gmail.com>
2021-10-03 08:55:56 +0300

    update

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -3,8 +3,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Home extends CI_Controller {
 
-	public function index()
+	public function login()
 	{
 		$this->load->view('login');
 	}
+
+  public function register()
+  {
+    $this->load->view('register');
+  }
 }
