4da81f167c5c2170c5f872a099530243d95847cf
Dana Sugu <dana.sugu@gmail.com>
2021-10-03 10:31:04 +0300

    update

---
+++ application/models/Orders_model.php
@@ -0,0 +1,10 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Orders_model extends CI_Model {
+
+	public function index()
+	{
+		$this->load->view('dashboard');
+	}
+}
