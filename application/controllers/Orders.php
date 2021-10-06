1c6fcddec38b67d73c8d115c0380c19968396de4
Dana Sugu <dana.sugu@gmail.com>
2021-10-06 07:54:15 +0300

    Update Orders.php

--- application/controllers/Orders.php
+++ application/controllers/Orders.php
@@ -5,6 +5,24 @@ class Orders extends CI_Controller {
 
 	public function index()
 	{
-		$this->load->view('orders');
+		$this->load->view('templates/header');
+		$this->load->view('savedorders');
+		$this->load->view('templates/footer');
+	}
+
+		public function payment ()
+	{
+		$this->load->view('templates/header');
+		$this->load->view('payment');
+    $this->load->view('templates/footer');
+
+	}
+		public function paymentsuccess ()
+	{
+		$this->load->view('templates/header');
+		$this->load->view('paymentsuccess');
+    $this->load->view('templates/footer');
+
 	}
 }
+
