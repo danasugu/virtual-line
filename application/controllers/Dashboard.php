7aa9536231427c89780ce3bc575180063faa095a
Dana Sugu <dana.sugu@gmail.com>
2021-10-06 07:16:46 +0300

    Update Dashboard.php

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -10,10 +10,17 @@ class Dashboard extends CI_Controller
         $this->load->view('templates/footer');
     }
 
-		 public function updateprofile()
+		public function updateprofile()
     {
         $this->load->view('templates/header');
         $this->load->view('updateprofile');
         $this->load->view('templates/footer');
     }
+		
+		public function upgrade()
+    {
+        $this->load->view('templates/header');
+        $this->load->view('upgrade');
+        $this->load->view('templates/footer');
+    }		
 }
