fa7c2a418a98d7502e6b1a08c81a7f2f0856bdbf
Dana Sugu <dana.sugu@gmail.com>
2021-10-07 08:07:40 +0300

    Update Home.php

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -14,10 +14,10 @@ class Home extends CI_Controller {
   {
 
     //process registration form
-    $this->form_validation->set_rules('name', 'Your Username', 'trim|required|min_lenght[3]');
+    $this->form_validation->set_rules('name', 'Your Username', 'trim|required|min_length[3]');
     $this->form_validation->set_rules('sex', 'Your Gender');
-    $this->form_validation->set_rules('password', 'Password', 'required|min_lenght[5]');
-    $this->form_validation->set_rules('confirm_password', 'Password Confirmation', 'required|min_lenght[5]');
+    $this->form_validation->set_rules('password', 'Password', 'required|min_length[5]');
+    $this->form_validation->set_rules('confirm_password', 'Password Confirmation', 'required|min_length[5]');
     $this->form_validation->set_rules('email', 'Email', 'trim|required');
 
     //check if all validation went thru
