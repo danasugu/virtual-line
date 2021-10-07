4bfe2912dbbe125c3da65c50a2d283343613e967
Dana Sugu <dana.sugu@gmail.com>
2021-10-07 10:11:13 +0300

    Update User_model.php

--- application/models/User_model.php
+++ application/models/User_model.php
@@ -2,6 +2,12 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class User_model extends CI_Model {
+
+  public function getUser($email)
+  {
+    $this->db->where(email', $email);  
+    $query = $this->db->get('users');  
+  }
  
   // public function getMessages()
   // {
