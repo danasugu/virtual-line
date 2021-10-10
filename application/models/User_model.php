020c1563bd66d458ef409dab464787e9ae28a662
Dana Sugu <dana.sugu@gmail.com>
2021-10-10 08:11:45 +0300

    Update User_model.php

--- application/models/User_model.php
+++ application/models/User_model.php
@@ -21,10 +21,10 @@ class User_model extends CI_Model {
   public function insertUserRegistrationData($email, $fullname, $sex, $password)
   {
       $data = array(
-        'fullname'  = $fullname;
-        'sex'          = $sex;
-        'password' = $password;
-        'email'       = $email;
+        'fullname'  => $fullname,
+        'sex'          => $sex,
+        'password' => $password,
+        'email'       => $email
       );
       $this->db->insert('users', $data);
       $insert_id =  $this->insert_id();
