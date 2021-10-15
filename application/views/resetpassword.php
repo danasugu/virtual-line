0f4b316145cdf5f134f918c186b17f1b9682d268
Dana Sugu <dana.sugu@gmail.com>
2021-10-15 08:19:34 +0300

    Update resetpassword.php

--- application/views/resetpassword.php
+++ application/views/resetpassword.php
@@ -1,13 +1,27 @@
 <div class="row">
       <div class="col-md-6 col-md-offset-3">
+      
+      
+      <?php if($this->session->flashdata('error')){
+    
+        echo '<div class="alert alert-danger alert-dismissable">
+      <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>' . $this->session->flashdata('error') . '</div>';
+    } ?>
+
+
+    <?php if($this->session->flashdata('success')){
+        echo
+         '<div class="alert alert-success alert-dismissable">
+      <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>'. $this->session->flashdata('success') . '</div>'; } ?>
+      
+      
       </div>
       <div class="col-md-4 col-md-offset-4" style="background-color: #fff; border: 1px solid #ddd; padding: 10px;">
       
-     
       <?php echo validation_errors('<div class="alert alert-danger alert-dismissable">
   <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>', '</div>'); ?>
-      <br>
-
+      
+       <br>
         <form class="form-horizontal" role="form" method="post" action="<?php echo base_url('home/resetpassword') ?>">
           <div class="form-group col-md-10">
             <label class="" for="email">Enter Email for Password Reset Code</label><br><br>
