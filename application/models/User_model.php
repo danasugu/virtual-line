b2b273bcfd7bbddbc19696ae23ee6423b8f08e3c
Dana Sugu <dana.sugu@gmail.com>
2021-10-16 08:35:03 +0300

    Update User_model.php

--- application/models/User_model.php
+++ application/models/User_model.php
@@ -1,108 +1,263 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class User_model extends CI_Model
-{
-    //check to see if the user exists
-    public function userExist($email)
+class User_model extends CI_Model {
+
+
+    //Checks to see if user already exist
+	public function userExist($email)
     {
         $this->db->where('email', $email);
-        
+
         $query = $this->db->get('users');
-        
+
         if($query->num_rows() > 0)
         {
             return TRUE;
-            
+
         }else
         {
             return FALSE;
         }
-    }
- 
-    // insert user registration data
 
+    }
+
+
+    //Insert User Registration Data
     public function insertUserRegistrationData($email, $fullname, $sex, $password)
     {
-        $data = array(
-        
+        $data = array (
+
         'fullname'  =>   $fullname,
         'sex'       =>   $sex,
         'password'  =>   $password,
         'email'     =>   $email
-        
+
         );
-        
+
         $this->db->insert('users', $data);
-        
+
         $insert_id = $this->db->insert_id();
-        
+
         return $insert_id;
-    }
-    
-    //process user login
-    public function getLoginData($email, $password)
-    {
-      $this->db->where('email', $email);
-        $this->db->where('password', $password);
 
-        $query ->$this->db->get('users');
 
-        if($query->num_rows() > 0)
-        {
-            return TRUE;
-            
-        }else
-        {
-            return FALSE;
-        }
     }
 
-        
+
+
     //Process User login
     public function verifyLoginData($email, $password)
     {
          $this->db->where('email', $email);
          $this->db->where('password', $password);
-        
+
          $query = $this->db->get('users');
-        
-        
+
+
         if($query->num_rows() > 0)
         {
             return TRUE;
-            
+
         }else
         {
             return FALSE;
         }
-        
+
 
     }
-    
-        public function getUserData($email)
+
+    //Get user info after login
+    public function getUserData($email)
     {
         $this->db->where('email', $email);
-        
+
         $query = $this->db->get('users');
-        
+
         return $query->row();
     }
 
-        public function userEmailExist($email)
+
+
+
+
+
+
+    //Checks to see if user already exist
+	public function userEmailExist($email)
     {
         $this->db->where('email', $email);
-        
+
         $query = $this->db->get('users');
-        
+
         if($query->num_rows() > 0)
         {
             return TRUE;
-            
+
         }else
         {
             return FALSE;
         }
+
     }
 
-  }
+
+
+
+    //Insert User Registration Data
+    public function insertPassResetData($data)
+    {
+
+        $this->db->insert('passreset', $data);
+
+        $insert_id = $this->db->insert_id();
+
+        return $insert_id;
+
+
+    }
+
+
+
+
+    //Checks to see if token and code valid
+	public function verifytoken($tokenid, $status)
+    {
+        $this->db->where('token', $tokenid);
+
+        $this->db->where('status', $status);
+
+        $query = $this->db->get('passreset');
+
+        $result = $query->row();
+
+        if(isset($result))
+        {
+            return $result->email;
+
+        }else
+        {
+            return FALSE;
+        }
+
+
+
+    }
+
+    //Checks to see if token and code valid
+	public function verifycode($code)
+    {
+        $this->db->where('code', $code);
+
+        $query = $this->db->get('passreset');
+
+        $result = $query->row();
+
+        if(isset($result))
+        {
+            return $result->email;
+
+        }else
+        {
+            return FALSE;
+        }
+
+
+
+    }
+
+
+
+    //Update new password
+    public function updateNewPassword($email, $password)
+    {
+        $this->db->where('email', $email);
+
+        $data = array (
+
+            'password'  =>   $password
+
+        );
+
+        $this->db->update('users', $data);
+
+        $result = $this->db->affected_rows();
+
+        return $result;
+
+    }
+
+
+    //Change stutas of password reset status to FALSE
+    public function updatePasswordResetStatus($email, $status)
+    {
+        $this->db->where('email', $email);
+
+        $data = array (
+
+            'status'  =>   $status
+
+        );
+
+        $this->db->update('passreset', $data);
+
+        $result = $this->db->affected_rows();
+
+        return $result;
+
+    }
+
+
+
+
+    //Function to delete user profile
+    public function deleteProfile($email)
+    {
+        $this->db->where('email', $email);
+
+        $query = $this->db->delete('users');
+
+        if($query)
+        {
+            return TRUE;
+        }else
+        {
+            return FALSE;
+        }
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
