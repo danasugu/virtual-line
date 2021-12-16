7c0ff28b04f3bc90e267470e5b813513ec82d963
Dana Sugu <dana.sugu@gmail.com>
2021-12-16 19:39:53 +0200

    update

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -4,23 +4,23 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Home extends CI_Controller {
 
-    
+
        //LOGIN ============================
 	public function login()
 	{
         if(isset($_SESSION['login']) == TRUE){
-         
+
             redirect('dashboard');
-            
+
         }else
-            
+
         {
-       
-       
+
+
         //Validate form input
         $this->form_validation->set_rules('email', 'Email', 'trim|required');
         $this->form_validation->set_rules('password', 'Password', 'required|min_length[5]');
-        
+
         if($this->form_validation->run() == FALSE)
         {
             $this->load->view('templates/header');
@@ -30,18 +30,18 @@ class Home extends CI_Controller {
         {
             $email      =   $this->input->post('email');
             $rawpass    =   $this->input->post('password');
-            
+
             $password   =   md5($rawpass);
-            
-            
+
+
             $result = $this->User_model->verifyLoginData($email, $password);
-            
+
             if($result == FALSE)
             {
                 $error = "User not Found. Please Register";
-                
+
                 $this->session->set_flashdata('error', $error);
-                
+
                 redirect('home/login');
             }else
             {
@@ -49,39 +49,39 @@ class Home extends CI_Controller {
                 $result = $this->User_model->getUserData($email);
 
                 $data = array(
-                    
+
                     'fullname'  =>   $result->fullname,
                     'image'     =>   $result->image,
                     'email'     =>   $result->email,
                     'id'        =>   $result->id,
                     'login'     =>   TRUE
-                
+
                 );
-  
+
                 $this->session->set_userdata($data);
 
                 redirect('dashboard');
-                
+
             }
-            
-            
-            
+
+
+
         }
-                 
+
       }//End of session checker
-        
+
 	}//End of login function
-    
-    
+
+
         //REGISTER  ============================
     public function register()
 	{
        if(isset($_SESSION['login']) == TRUE){
-         
+
             redirect('dashboard');
-            
+
         }else
-            
+
         {
         //Proccess registration form
         $this->form_validation->set_rules('sex', 'Your Sex');
@@ -89,8 +89,8 @@ class Home extends CI_Controller {
         $this->form_validation->set_rules('password', 'Password', 'required|min_length[5]');
         $this->form_validation->set_rules('confirm_password', 'Password Confirmation', 'required|min_length[5]');
         $this->form_validation->set_rules('email', 'Email', 'trim|required');
-        
-        
+
+
         if($this->form_validation->run() == FALSE)
         {
             $this->load->view('templates/header');
@@ -103,75 +103,75 @@ class Home extends CI_Controller {
             $sex        =   $this->input->post('sex');
             $email      =   $this->input->post('email');
             $rawpass    =   $this->input->post('confirm_password');
-            
-            
+
+
             //Verify if user exist
-            
+
             $result = $this->User_model->userExist($email);
-            
-            
+
+
             if($result == TRUE)
             {
                 $error = "User Already Exist, Login";
-                
+
                 $this->session->set_flashdata('error', $error);
-                
+
                 redirect('home/login');
-                
+
             }else
             {
                 //Insert Data in Database
-                
+
                 $password   =   md5($rawpass);
-                
+
                 $result = $this->User_model->insertUserRegistrationData($email, $fullname, $sex, $password);
-                
+
                 if($result > 0)
                 {
                     $success = "Registration Successfull, Login";
-                
+
                     $this->session->set_flashdata('success', $success);
-                    
+
                     redirect('home/login');
-                    
+
                 }else
                 {
-                
+
                     $error = "Registration not Successful, Try Again";
-                
+
                     $this->session->set_flashdata('error', $error);
-                    
+
                     redirect('home/register');
                 }
-                
-                
+
+
             }
-              
-            
+
+
            }
-        
+
         }
-        
+
 	}
-    
-    
-    
+
+
+
         //RESET PASSWORD ============================
     //Getting user email for password reset
      public function resetpassword()
 	{
          if(isset($_SESSION['login']) == TRUE){
-         
+
             redirect('dashboard');
-            
+
         }else
-            
+
         {
-        
-        
+
+
         //validating email input
         $this->form_validation->set_rules('email', 'Email', 'trim|required');
-        
+
         if($this->form_validation->run() == FALSE)
         {
             $this->load->view('templates/header');
@@ -179,36 +179,36 @@ class Home extends CI_Controller {
             $this->load->view('templates/footer');
         }else
         {
-            
+
             //Get email
             $email = $this->input->post('email');
 
             //Check if email exist
             $result = $this->User_model->userEmailExist($email);
-            
+
             $this->session->set_userdata('email', $email);
-            
+
             if($result == TRUE)
             {
                 //Email the user
-            
+
                 $token      =   md5(uniqid(rand(), true));
-                
+
                 $randcode   =   md5($email);
-                
+
                 $code       =   substr($randcode, 2,8);
-                
+
                 $status     =   "TRUE";
-                
-                
+
+
                 $subject    =   "Password Reset Link | Virtualines";
-                
+
                 $message    =   "Dear Customer,\r\n You requested for a password reset on Virtualine Platform. \r\nKindly click on the link or copy and paste this link in your browser url to reset your password.\n\n This is your Link : ". base_url('home/verifytoken')."/?tokenID=" . $token . "&status=" . $status . " \n\nYour Reset Password Code is : " . $code . " \r\nThank You\r\nRegards, \r\nVirtualines Support \r\ninfo@virtualines.com";
-            
-                
+
+
                 //Setting email config
                 $config = Array(
-                
+
                     'protocol'      =>      'smtp',
                     'smtp_host'     =>      'ssl://smtp.googlemail.com',
                     'smtp_port'     =>      465,
@@ -217,98 +217,98 @@ class Home extends CI_Controller {
                     'mailtype'      =>      'html',
                     'charset'       =>      'iso-8859-1',
                     'wordwrap'      =>      TRUE
-                
+
                 );
                 //Load library and pass in the config
                 $this->load->library('email', $config);
                 $this->email->set_newline('\r\n');
-                
+
                 $supportEmail   =   "reset@virtualine.com";
                 $supportName    =   "Support Team";
                 $email          =   $this->session->userdata('email');
-                
+
                 $this->email->from($supportEmail, $supportName);
                 $this->email->to($email);
-                
+
 
                 $this->email->subject($subject);
                 $this->email->message($message);
 
                 if($this->email->send())
                 {
-                   
+
                     //send data to the table
                     $data = array(
-                    
+
                         'email'     =>   $email,
                         'token'     =>   $token,
                         'code'      =>   $code,
                         'status'    =>   "TRUE"
-                    
+
                     );
-                    
+
                     //Call the model function to insert data in the reset password table
                     $result = $this->User_model->insertPassResetData($data);
-                    
+
                     if($result > 0)
                     {
                         $success = "Please check you email for password reset code";
-                        
+
                         $this->session->set_flashdata('success', $success);
-                        
+
                         redirect('home/login');
                     }
-                    
-                    
-                    
+
+
+
                 }else
                 {
                     $error = "Message not sent. Email not Valid. Re-enter Email";
-                    
+
                     $this->session->set_flashdata('error', $error);
-                    
+
                     redirect('home/resetpassword');
                 }
 
 
-                
+
             }else
             {
                 $error = "Email not Valid.... Re-enter Email";
-                    
+
                 $this->session->set_flashdata('error', $error);
-                    
+
                 redirect('home/resetpassword');
-                
+
             }
-    
+
            }
-            
+
         }
-        
+
 	}
-    
-    
+
+
      public function verifytoken()
 	{
          if(isset($_SESSION['login']) == TRUE){
-         
+
             redirect('dashboard');
-            
+
         }else
-            
+
         {
-            
+
         $url        =   parse_url($_SERVER['REQUEST_URI']);
                         parse_str($url['query'], $params);
-        
+
         $tokenid    =   $params['tokenID'];
-        
+
         $status     =   $params['status'];
-        
-        
+
+
         //Check if the code and token and status are valid
-           
+
         $result = $this->User_model->verifytoken($tokenid, $status);
 
         if($result == false)
@@ -322,7 +322,7 @@ class Home extends CI_Controller {
         }else
         {
             $userEmail = $result;
-            
+
             $this->session->set_userdata('userEmail', $userEmail);
 
             $success = "Your Token has been verified For " . $userEmail . " Please Enter Code";
@@ -332,27 +332,27 @@ class Home extends CI_Controller {
             redirect('home/verifypasswordcode');
 
         }
-        
-        
+
+
         }
-        
-       
+
+
 	}
-    
+
         //VERIFY PASSWORD ============================
     public function verifypasswordcode()
     {
-        
+
         if(isset($_SESSION['login']) == TRUE){
-        
+
             redirect('dashboard');
-            
+
         }else
-            
+
         {
-        
+
         $this->form_validation->set_rules('resetcode', 'Reset Code', 'trim|required|min_length[7]');
-        
+
         if($this->form_validation->run() == FALSE)
         {
             $this->load->view('templates/header');
@@ -361,13 +361,13 @@ class Home extends CI_Controller {
         }else
         {
             $code = $this->input->post('resetcode');
-            
+
             $result = $this->User_model->verifycode($code);
-            
+
             if($result)
             {
                 redirect('home/newpassword');
-                
+
             }else
             {
                 $error = "Sorry, Password Reset Code Invalid. Try Again";
@@ -376,29 +376,24 @@ class Home extends CI_Controller {
 
                 redirect('home/resetpassword');
             }
-            
-            
         }
-            
       }
-    
-    
     }
-    
+
     //NEW PASSWORD ============================
       public function newpassword()
 	{
          if(isset($_SESSION['login']) == TRUE){
-         
+
             redirect('dashboard');
-            
+
         }else
-            
+
         {
 
         $this->form_validation->set_rules('password', 'Password', 'required|min_length[5]');
         $this->form_validation->set_rules('confirm_password', 'Password Confirmation', 'required|min_length[5]');
-        
+
         if($this->form_validation->run() == FALSE)
         {
             $this->load->view('templates/header');
@@ -407,49 +402,47 @@ class Home extends CI_Controller {
         }else
         {
             $rawpass    =   $this->input->post('confirm_password');
-            
+
             $password   =   md5($rawpass);
-            
+
             $email      =   $this->session->userdata('userEmail');
-            
-            
+
+
             $result = $this->User_model->updateNewPassword($email, $password);
-            
+
             if($result > 0)
             {
                 //Change the status in the password reset table to FALSE
                 $status     = "FALSE";
                 $email      = $this->session->userdata('userEmail');
                 $result     = $this->User_model->updatePasswordResetStatus($email, $status);
-                
+
                 if($result > 0){
-                    
+
                      $success = "Your Password Successfully Changed. Please Login";
 
                     $this->session->set_flashdata('success', $success);
 
                     redirect('home/login');
                 }
- 
+
             }
-            
+
           }
-        
+
         }
-        
-        
 	}
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -1,7 +1,7 @@
 <html>
 
     <head>
-        
+
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -23,7 +23,7 @@
         <link href="<?php echo base_url("assets/font-awesome/css/font-awesome.min.css"); ?>" rel="stylesheet" type="text/css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 
-        
+
 
         <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
         <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -34,8 +34,8 @@
         url(https://lh3.googleusercontent.com/-7kOBhr3B2dE/AAAAAAAAAAI/AAAAAAAAAAA/AOtt-yHs4g14qqNJaJBXAcpIMv_fV9dDGw/s32-c-mo/photo.jpg)
         -->
     </head>
-    
-    <body style="padding-top: 50px;">            
+
+    <body style="padding-top: 50px;">
     <nav class="navbar navbar-default navbar-fixed-top navbar-inverse">
 
         <div class="container-fluid" >
@@ -55,51 +55,51 @@
                 <ul class="nav navbar-nav navbar-left">
                     <li><a href=""></a></li>
                 </ul>
-                
-         
+
+
 
                 <ul class="nav navbar-nav navbar-right">
-                 <li class="navbar-text">Welcome fullname here </li>
+                 <li class="navbar-text">Welcome <?= $this->session->userdata('fullname'); ?> </li>
                   <li class="dropdown">
-    
+
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown">
                        <?php echo 'image here' ?>
                         </a>
                                 <ul class="dropdown-menu">
                                     <li><a href=""><i class="fa fa-dashboard"></i> Account</a></li>
                                     <li class="divider"></li>
-                                    
+
                                      <li><a data-toggle="modal" data-target="#settings"><i class="fa fa-cog"></i> Settings</a></li>
                                     <li class="divider"></li>
                                     <li><a href=""><i class="fa fa-sign-out"></i> Sign-out</a></li>
                                 </ul>
                     </li>
                 </ul>
-              
+
             </div>
 
         </div>
 
     </nav>
-        
+
     <div class="container-fluid">
-    
+
             <!-- Modal Settings-->
                     <div id="settings" class="modal fade" role="dialog">
                       <div class="modal-dialog">
 
                         <!-- Modal content-->
                         <div class="modal-content">
-                        
+
                           <div class="modal-header">
                             <button type="button" class="close" data-dismiss="modal">&times;</button>
                             <h4 class="modal-title">Account Settings </h4>
                             <h4 class="modal-title" style="color: red">* Note that only Admin can undo changes </h5>
                           </div>
                           <div class="modal-body">
-                        
+
                            <form id ='registration', role = 'form', action="">
-                              
+
                               <div class="form-group">
                                     <label class="checkbox-inline"><span class="required"></span><input type="checkbox" value="2" name="no_cert">Do not Show My Certification</label>
                                 </div>
@@ -109,36 +109,35 @@
                                 <div class="form-group">
                                     <label class="checkbox-inline"><span class="required"></span><input type="checkbox" value="2" name="no_mem_reason">Do not Show My Membership Reason</label>
                                 </div>
-                                
+
 
                                 <div class="form-group">
-                                    
+
                                     <button type="submit" id="contact_form_submit" name="update_settings" class="btn btn-default">Submit</button>
                                 </div>
                             </form>
-                        
+
                           </div>
                           <div class="modal-footer">
                             <a href="" class="btn btn-danger">Delete Account</a>
                             <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
                           </div>
                         </div>
-                      
+
 
                       </div>
                     </div>
-                    
-                    
 
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+
+
+
+
+
+
+
+
+
+
+
+
+
