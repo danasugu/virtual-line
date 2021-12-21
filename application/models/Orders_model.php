a03a7b16ad6857454bef23508597cc0dba275f97
Dana Sugu <dana.sugu@gmail.com>
2021-12-21 07:22:17 +0200

    Update Orders_model.php

--- application/models/Orders_model.php
+++ application/models/Orders_model.php
@@ -3,12 +3,21 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Orders_model extends CI_Model {
 
-  // save order price
+ //Function to get total saved order price
+ public function getSavedOrder($email)
+ {
 
-  public function getSavedOrder($email)
-  {
+ $query = $this->db->query("SELECT * FROM cart WHERE email='$email'ORDER BY date DESC");
 
-  }
+ foreach($query->result() as $row)
+ {
+ $itemPrice = $row->item_price;
+
+ return $itemPrice;
+ }
+
+ }
+
 
   //fetch all products
 
